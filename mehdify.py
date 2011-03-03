e4c918b2a283203fe5c5d67763e733b3266e8727
Mehdi Sdwilsh <mehdisdwilsh@gmail.com>
2011-03-03 13:44:42 -0500

    initial add

---
+++ mehdify.py
@@ -0,0 +1,25 @@
+def mehdify(status):
+  replace_dict = { 'awesome': 'awesome/terrible',
+                   'terrible': 'awesome/terrible',
+                   'best': 'best/worst',
+                   'worst': 'best/worst',
+                   'better': 'better/worse',   
+                   'worse': 'better/worse',
+                   'love': 'love/hate',
+                   'hate': 'love/hate'}
+
+  # replace some adjectives
+  words = status.split(' ')
+  for word in words:
+    if word in replace_dict:
+      status = status.replace(word, replace_dict[word])
+
+  # add ", bro" to @replies
+  if status[0] is '@':
+    return re.sub(r'[?!,.]*$', ', bro', status)
+                   
+  # prepend "aww yeah" or "whoa" to some tweets
+  if random.random() > 0.5:
+    return 'aww yeah, ' + status
+                   
+  return 'whoa, ' + status
